ea40ecee4c7fb6068194694d079ac2bd16571d8a
andrea285 <andrea.lojpur285@gmail.com>
2025-07-14 19:01:32 +0200

    Creating & Using a Data Model

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+// export type InvestmentInput = {
+//   initialInvestment: number;
+//   duration: number;
+//   expectedReturn: number;
+//   annualInvestment: number;
+// };
+
+export interface InvestmentInput {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
